b435eb7dd8e3b48d97377db20ab68a8fd0908a85
kukymbr <sergey@espressio.ru>
2021-06-17 18:59:18 +0300

    add Input interface

--- models.go
+++ models.go
@@ -4,3 +4,12 @@ package bubucore
 type Ok struct {
 	Ok bool `json:"ok"`
 }
+
+// Input is a filterable and validatable input
+type Input interface {
+	// Filter filters input values
+	Filter()
+
+	// Validate checks if input values are valid
+	Validate() error
+}
