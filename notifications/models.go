85ac1ab05c118e32291d949686cf8bcf1abee72a
kukymbr <sergey@espressio.ru>
2021-06-21 15:33:21 +0300

    [notifications] custom email - single struct

--- notifications/models.go
+++ notifications/models.go
@@ -12,74 +12,40 @@ type PlainText struct {
 	Text string `json:"text"`
 }
 
-// Email interface
-type Email interface {
-	GetRecipients() ([]*mail.Address, error)
-}
-
-// EmailPlain is a plain text email notification
-type EmailPlain struct {
+// Email notification
+type Email struct {
 	Subject    string   `json:"subject"`
-	Text       string   `json:"text"`
 	Recipients []string `json:"recipients"`
-}
 
-// Filter filters notification values
-func (n *EmailPlain) Filter() {
-	n.Subject = strings.TrimSpace(n.Subject)
-	n.Text = strings.TrimSpace(n.Text)
-}
+	Text string `json:"text,omitempty"`
 
-// Validate checks if values are OK
-func (n *EmailPlain) Validate() error {
-	n.Filter()
-	if n.Subject == "" {
-		return bubucore.NewError(http.StatusBadRequest, "no subject given")
-	}
-	if n.Text == "" {
-		return bubucore.NewError(http.StatusBadRequest, "no text given")
-	}
-	if len(n.Recipients) == 0 {
-		return bubucore.NewError(http.StatusBadRequest, "empty recipients list given")
-	}
-	return nil
-}
-
-// GetRecipients prepares recipients list
-func (n *EmailPlain) GetRecipients() ([]*mail.Address, error) {
-	return mail.ParseAddressList(strings.Join(n.Recipients, ", "))
-}
-
-// EmailWithTemplate is an email notification
-type EmailWithTemplate struct {
-	Subject        string      `json:"subject"`
 	TemplateName   string      `json:"template_name,omitempty"`
 	TemplateValues interface{} `json:"template_values,omitempty"`
-	Recipients     []string    `json:"recipients"`
 }
 
 // Filter filters notification values
-func (n *EmailWithTemplate) Filter() {
+func (n *Email) Filter() {
 	n.Subject = strings.TrimSpace(n.Subject)
+	n.Text = strings.TrimSpace(n.Text)
 	n.TemplateName = strings.TrimSpace(n.TemplateName)
 }
 
 // Validate checks if values are OK
-func (n *EmailWithTemplate) Validate() error {
+func (n *Email) Validate() error {
 	n.Filter()
 	if n.Subject == "" {
 		return bubucore.NewError(http.StatusBadRequest, "no subject given")
 	}
-	if n.TemplateName == "" {
-		return bubucore.NewError(http.StatusBadRequest, "no template name given")
-	}
 	if len(n.Recipients) == 0 {
 		return bubucore.NewError(http.StatusBadRequest, "empty recipients list given")
+	}
+	if n.Text == "" && n.TemplateName == "" {
+		return bubucore.NewError(http.StatusBadRequest, "no text or template name given")
 	}
 	return nil
 }
 
 // GetRecipients prepares recipients list
-func (n *EmailWithTemplate) GetRecipients() ([]*mail.Address, error) {
+func (n *Email) GetRecipients() ([]*mail.Address, error) {
 	return mail.ParseAddressList(strings.Join(n.Recipients, ", "))
 }
