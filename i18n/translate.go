f592033133a80fb660849544d7e796d7b03da466
kukymbr <sergey@espressio.ru>
2021-06-30 20:37:39 +0300

    [i18n] remove translate functions

--- i18n/translate.go
+++ i18n/translate.go
@@ -4,17 +4,3 @@ package i18n
 type Translatable interface {
 	Translate(lang Language)
 }
-
-// TranslateSlice calls Translate on each Translatable item in slice
-func TranslateSlice(items []Translatable, lang Language) {
-	for _, item := range items {
-		item.Translate(lang)
-	}
-}
-
-// TranslateStringMap calls Translate on each Translatable item in string map
-func TranslateStringMap(items map[string]Translatable, lang Language) {
-	for _, item := range items {
-		item.Translate(lang)
-	}
-}
