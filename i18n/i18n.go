f4d1d49838847f25674b7a640004e7c3ba408f0b
kukymbr <sergey@espressio.ru>
2021-07-02 14:15:14 +0300

    [i18n] filter/validate Language

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -1,5 +1,9 @@
 package i18n
 
+import (
+	"strings"
+)
+
 // Available languages
 const (
 	LangRu = Language("ru")
@@ -14,6 +18,16 @@ var Source = &TextsSource{
 
 // Language is a language code
 type Language string
+
+// Filter value
+func (l *Language) Filter() {
+	*l = Language(strings.TrimSpace(string(*l)))
+}
+
+// Validate if value is ok
+func (l Language) Validate() error {
+	return nil
+}
 
 // T is a Translations.GetText alias
 func T(textOrKey string, lang Language, tplData interface{}) string {
