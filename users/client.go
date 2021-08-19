ef4d254c5de4729e6642b71a11b472e0047eb62b
kukymbr <sergey@espressio.ru>
2021-08-19 15:13:14 +0300

    [users] add bubulearn users client

---
+++ users/client.go
@@ -0,0 +1,102 @@
+package users
+
+import (
+	"bytes"
+	"github.com/bubulearn/bubucore"
+	jsoniter "github.com/json-iterator/go"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+)
+
+const logTag = "[bubucore][users]"
+
+const endpointUserInfo = "auth/user/"
+
+// Client is a bubulearn users service client
+type Client struct {
+	host    string
+	token   string
+	_client *http.Client
+}
+
+// GetUserInfo fetches user info by user ID
+func (c *Client) GetUserInfo(userID string) (user *User, err error) {
+	err = c.doRequest(http.MethodGet, endpointUserInfo+userID, nil, &user)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// doRequest sends request to the gorush and decodes response to the respData
+func (c *Client) doRequest(method string, endpoint string, reqData interface{}, respData interface{}) (err error) {
+	err = c.checkPreconditions()
+	if err != nil {
+		return err
+	}
+
+	endpoint = "/" + strings.TrimLeft(endpoint, "/")
+
+	body := []byte("")
+	if reqData != nil {
+		body, err = jsoniter.Marshal(reqData)
+		if err != nil {
+			return err
+		}
+	}
+
+	req, err := http.NewRequest(method, c.host+endpoint, bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.token)
+	req.Header.Set("Content-type", "application/json")
+
+	resp, err := c.client().Do(req)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return bubucore.NewError(resp.StatusCode, "[gorush] non-OK response: "+string(body))
+	}
+
+	err = jsoniter.Unmarshal(body, respData)
+	if err != nil {
+		return bubucore.NewError(http.StatusBadGateway, "[gorush] failed to decode response: "+string(body))
+	}
+
+	return nil
+}
+
+// checkPreconditions validates if Client data is ok
+func (c *Client) checkPreconditions() error {
+	if c.host == "" {
+		return bubucore.NewError(http.StatusInternalServerError, logTag+" no host defined")
+	}
+	if c.token == "" {
+		return bubucore.NewError(http.StatusInternalServerError, logTag+" no token defined")
+	}
+	return nil
+}
+
+// client returns http.Client instance
+func (c *Client) client() *http.Client {
+	if c._client == nil {
+		c._client = &http.Client{
+			Timeout: 10 * time.Second,
+		}
+	}
+	return c._client
+}
---
+++ users/user.go
@@ -0,0 +1,76 @@
+package users
+
+import (
+	"github.com/bubulearn/bubucore"
+	"github.com/bubulearn/bubucore/i18n"
+	"net/mail"
+	"time"
+)
+
+// User represents a common user data, students and teachers both
+type User struct {
+	ID string `json:"id" example:"4452dda6-4fde-453f-a41d-4c043e0ea6d1"`
+
+	// Role ID, 1=student, 500=teacher, 1000=admin
+	Role  int    `json:"role" example:"500"`
+	Email string `json:"email" example:"john@example.com"`
+
+	Name  string `json:"name" example:"John Doe"`
+	Phone string `json:"phone" example:"+7 (900) 123-45-67"`
+
+	// Gender ID, 0=unset, 1=male, 2=female
+	Gender byte `json:"gender" example:"1"`
+
+	// Code for teacher to assign to the student
+	Code string `json:"code" example:"123456"`
+
+	Language i18n.Language `json:"lang" swaggertype:"string" enums:"en,ru" example:"ru"`
+
+	// TeacherID for student, ID of teacher assigned
+	TeacherID string `json:"teacher_id,omitempty" example:"b21b949e-8495-4f56-ab9e-502199af48cf"`
+
+	IsBlocked     bool   `json:"is_blocked" example:"false"`
+	BlockedReason string `json:"blocked_reason,omitempty" example:"Really bad ass"`
+
+	TimeCreated   *time.Time `json:"time_created" example:"2021-07-12T20:29:40+03:00"`
+	TimeUpdated   *time.Time `json:"time_updated" example:"2021-07-12T20:29:40+03:00"`
+	TimeLastLogin *time.Time `json:"time_last_login" example:"2021-07-12T20:29:40+03:00"`
+
+	// List of assigned students to the teacher
+	StudentsAssigned []*User `json:"students_assigned,omitempty"`
+
+	// DeviceTokens is a list of user's registered devices tokens
+	DeviceTokens []string `json:"device_tokens"`
+}
+
+// IsStudent checks if user has a student role
+func (u *User) IsStudent() bool {
+	return u.Role == RoleStudent
+}
+
+// IsTeacher checks if user has a teacher role
+func (u *User) IsTeacher() bool {
+	return u.Role == RoleTeacher
+}
+
+// IsAdmin checks if user has an admin role
+func (u *User) IsAdmin() bool {
+	return u.Role == RoleAdmin
+}
+
+// GetEmailAddress return user's name and email as email address instance
+func (u *User) GetEmailAddress() *mail.Address {
+	return &mail.Address{
+		Address: u.Email,
+		Name:    u.Name,
+	}
+}
+
+// ValidateUnblocked check if user is not blocked.
+// Returns common.ErrUserBlocked in case of block.
+func (u *User) ValidateUnblocked() error {
+	if u.IsBlocked {
+		return bubucore.ErrUserBlocked
+	}
+	return nil
+}
