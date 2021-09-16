4fd7b1b4a774ff9ebd7b226a67aabc7899bd3e81
kukymbr <sergey@espressio.ru>
2021-09-16 13:07:31 +0300

    [app] graceful shutdown

--- app/app.go
+++ app/app.go
@@ -1,10 +1,13 @@
 package app
 
 import (
+	"context"
 	"github.com/bubulearn/bubucore/di"
 	"github.com/bubulearn/bubucore/ginsrv"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
+	"os"
+	"os/signal"
 )
 
 const logTag = "[bubucore.App] "
@@ -65,6 +68,28 @@ func (a *App) Init() {
 func (a *App) Run() {
 	a.Init()
 	defer a.Close()
+
+	// Graceful shutdown
+	{
+		ctx := context.Background()
+		ctx, cancel := context.WithCancel(ctx)
+
+		osSignalCh := make(chan os.Signal, 1)
+		signal.Notify(osSignalCh, os.Interrupt)
+
+		defer func() {
+			signal.Stop(osSignalCh)
+			cancel()
+		}()
+
+		go func() {
+			select {
+			case <-osSignalCh:
+				cancel()
+			case <-ctx.Done():
+			}
+		}()
+	}
 
 	router := DIGetRouter(a.ctn)
 	conf := DIGetConfig(a.ctn)
